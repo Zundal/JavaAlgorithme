1b307cfc4361a2632cdd1bdcaac0eeda240c1075
Zundal <ajs15010120@gmail.com>
2022-05-29 12:14:55 +0900

    알고리즘 v2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
-// TODO: 효율성 수정필요
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/*
+    TODO: 효율성 수정필요
+        - [ ] 비교줄이기
+*/
 public class App {
     public static void main(String[] args) throws Exception {
         Solution a = new Solution();
@@ -18,26 +21,27 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        List<String> list1 = new ArrayList<String>();
-        Collections.addAll(list1, participant);
+        List<String> ArrayListParticipant = new ArrayList<String>();
+        Collections.addAll(ArrayListParticipant, participant);
 
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        // Arrays.sort(participant);
+        // Arrays.sort(completion);
         
         for (String ele : completion) {
             boolean flag = false;
             for (String el : participant) {
-                if(flag == true){
+                if(flag == true) {
                     continue;
                 }
                 if(ele.equals(el)) {
-                    if(list1.remove(ele) == true){
+                    if(ArrayListParticipant.remove(ele) == true){
                         flag = true;
                     }
                 }
             }
         }
-        for(String ell:list1){
+        
+        for(String ell:ArrayListParticipant){
             answer = ell;
         }
 
